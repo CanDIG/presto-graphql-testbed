c796142ad609e2a2fa0e5cf986263caa82b3d285
Jonathan Dursi <jonathan@dursi.ca>
2019-03-09 13:23:52 -0500

    Have graphql service retry connection until mongodb is up

--- graphql_service/server.js
+++ graphql_service/server.js
@@ -1,23 +1,24 @@
+const child_process = require("child_process");
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const graphqlExpress = require("express-graphql");
 const bookSchema = require('./graphql/BookSchema').BookSchema;
 
-mongoose.connect('mongodb://mongo/myappdb', (err) => {
-    if (err) throw err;
-    console.log("connected to mongo");
-})
-
-
-
+var connectWithRetry = function() {
+  return mongoose.connect('mongodb://mongo/myappdb', function(err) {
+    if (err) {
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      setTimeout(connectWithRetry, 5000);
+    }
+  });
+};
+connectWithRetry();
 
 app.set('port', (process.env.PORT || 4000));
 app.listen(app.get('port'),  () =>{
     console.log("Node app is running at localhost:" + app.get('port'))
 });
-
-
 
 app.use('/graphql', graphqlExpress({
     schema: bookSchema,
