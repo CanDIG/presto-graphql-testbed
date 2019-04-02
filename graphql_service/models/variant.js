90545e2689b916a2245b35da4299bd26bccdd5d5
Matt Wong <matt@godfreds-air.sickkids.ca>
2019-04-02 14:09:30 -0400

    Update mysql lib to support mysql 8

--- graphql_service/models/variant.js
+++ graphql_service/models/variant.js
@@ -1,26 +1,29 @@
-var mysql = require('mysql')
-var con = mysql.createConnection({
+var mysqlx = require('@mysql/xdevapi')
+var options = {
   host: "mysql",
   user: "root",
   password: "rootpass",
-  database: "var_db",
-  port: "3306",
-  connectTimeout: 100000,
-});
-
-con.connect();
+  port: "33060",
+  schema: 'var_db' // an error is thrown if it does not exist
+};
 
 // searches first 5 variants and returns it
 exports.find = () => {
     return new Promise(resolve => {
-        var variants;
-        con.query('select variant_id, chrom, start, end, ref, alt, gene, polyphen_score, af_exac_all from variants limit 10', function (error, results, fields) {
-            if (error) {
+        var variants = [];
+        var cols = ['variant_id', 'vcf_id', 'chrom', 'start', 'end', 'ref', 'alt', 'gene' ,'polyphen_score', 'af_exac_all'];
+        var col_str = cols.join(', ');
+        mysqlx.getSession(options)
+            .then(function (session) {
+                session.sql('select ' + col_str + ' from variants limit 10').execute(function(row) {
+                    row_json = {}
+                    cols.forEach((key, i) => row_json[key] = row[i]);
+                    variants.push(row_json)
+                    resolve(variants);
+                });
+            }).catch(function(error) {
                 console.log(error);
                 throw error;
-            }
-            variants = JSON.parse(JSON.stringify(results));
-            resolve(variants);
-        });
+            });
     });
 }
